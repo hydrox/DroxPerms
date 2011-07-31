844b017c25c89daeb6fb1a81e749e0ead6c62af5
Matthias Söhnholz <miniminko@googlemail.com>
2011-07-31 16:46:51 +0200

    add world-permissions mirroring

--- src/main/java/de/hydrox/bukkit/DroxPerms/data/Config.java
+++ src/main/java/de/hydrox/bukkit/DroxPerms/data/Config.java
@@ -3,16 +3,19 @@ package de.hydrox.bukkit.DroxPerms.data;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.Plugin;
 import org.bukkit.util.config.Configuration;
+import org.bukkit.util.config.ConfigurationNode;
 
 import de.hydrox.bukkit.DroxPerms.data.flatfile.FlatFilePermissions;
 
 public class Config {
 	private static String dataProvider = null;
 	private static String defaultWorld = null;
+	private static HashMap<String, ArrayList<String>> worldMirrors= null;
 	private Configuration configuration = null;
 	private Logger logger;
 	
@@ -28,14 +31,28 @@ public class Config {
             ArrayList<String> defaultMirror = new ArrayList<String>();
             defaultMirror.add("nether");
             mirrors.put("world", defaultMirror);
+            mirrors.put("hub", null);
             configuration.setProperty("DataProvider", FlatFilePermissions.NODE);
             configuration.setProperty("DefaultWorld", "world");
             configuration.setProperty("Mirrors", mirrors);
             configuration.save();
         }
+		logger.info("[DroxPerms] loading configuration");
+		configuration.load();
+
 		dataProvider = configuration.getString("DataProvider");
-		defaultWorld = configuration.getString("DefaultWorld");
 		logger.info("[DroxPerms] Using DataProvider: " + dataProvider);
+		defaultWorld = configuration.getString("DefaultWorld");
+		logger.info("[DroxPerms] Setting DefaultWorld: " + defaultWorld);
+		logger.info("[DroxPerms] Loading World-Mirrors");
+		worldMirrors = new HashMap<String, ArrayList<String>>();
+		ConfigurationNode tmp = configuration.getNode("Mirrors");
+		Iterator<String> iter = tmp.getKeys().iterator();
+		while (iter.hasNext()) {
+			String world = iter.next();
+			worldMirrors.put(world, (ArrayList<String>) tmp.getStringList(world, new ArrayList<String>()));
+			logger.fine("mirrors for world "+world+": " + worldMirrors.get(world).size());
+		}
 		
 		// TODO Needs config-file reader
 	}
@@ -45,6 +62,22 @@ public class Config {
 	}
 
 	public static String getDefaultWorld() {
+		return defaultWorld;
+	}
+
+	public static String getRealWorld(String world) {
+		if (worldMirrors.containsKey(world)) {
+			return world;
+		}
+		for (String key : worldMirrors.keySet()) {
+			Iterator<String> iter = worldMirrors.get(key).iterator();
+			while (iter.hasNext()) {
+				String string = (String) iter.next();
+				if (string.equals(world)) {
+					return key;
+				}
+			}
+		}
 		return defaultWorld;
 	}
 }
