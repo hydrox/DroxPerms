9d8cb7b3baf77ffecca71c228ead917b5d705fb6
Matthias Söhnholz <miniminko@googlemail.com>
2011-08-23 11:14:48 +0200

    JavaDoc

--- src/main/java/de/hydrox/bukkit/DroxPerms/DroxPermsAPI.java
+++ src/main/java/de/hydrox/bukkit/DroxPerms/DroxPermsAPI.java
@@ -9,94 +9,283 @@ import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.bukkit.plugin.Plugin;
 
-
 public class DroxPermsAPI {
 	DroxPerms plugin = null;
 	FakeCommandSender fakeCS = null;
-	
+
 	public DroxPermsAPI(DroxPerms plugin) {
 		this.plugin = plugin;
 		this.fakeCS = new FakeCommandSender();
 	}
 
+	/**
+	 * Returns the Group of a Player.
+	 * 
+	 * @param player
+	 *            Player to be queried
+	 * @return Group-name
+	 */
 	public String getPlayerGroup(String player) {
 		return plugin.dataProvider.getPlayerGroup(player);
 	}
 
+	/**
+	 * Sets a Players Group.
+	 * 
+	 * @param player
+	 *            Player to be modified
+	 * @param group
+	 *            Group to be set
+	 * @return success of operation
+	 */
 	public boolean setPlayerGroup(String player, String group) {
 		return plugin.dataProvider.setPlayerGroup(fakeCS, player, group);
 	}
 
+	/**
+	 * Returns all Subgroups of a given Player.
+	 * 
+	 * @param player
+	 *            Player to be queried
+	 * @return Array of Subgroup-Strings
+	 */
 	public String[] getPlayerSubgroups(String player) {
 		return plugin.dataProvider.getPlayerSubgroups(player);
 	}
 
+	/**
+	 * Adds a Subgroup to a Player.
+	 * 
+	 * @param player
+	 *            Player to be modified
+	 * @param subgroup
+	 *            Subgroup to be added
+	 * @return success of operation
+	 */
 	public boolean addPlayerSubgroup(String player, String subgroup) {
 		return plugin.dataProvider.addPlayerSubgroup(fakeCS, player, subgroup);
 	}
 
+	/**
+	 * Removes a Subgroup from a Player.
+	 * 
+	 * @param player
+	 *            Player to be modified
+	 * @param subgroup
+	 *            Subgroup to be removed
+	 * @return success of operation
+	 */
 	public boolean removePlayerSubgroup(String player, String subgroup) {
-		return plugin.dataProvider.removePlayerSubgroup(fakeCS, player, subgroup);
+		return plugin.dataProvider.removePlayerSubgroup(fakeCS, player,
+				subgroup);
 	}
 
+	/**
+	 * Adds a global Permission to a Player.
+	 * 
+	 * @param player
+	 *            Player to be modified
+	 * @param node
+	 *            Permission-node to be added
+	 * @return success of operation
+	 */
 	public boolean addPlayerPermission(String player, String node) {
 		return addPlayerPermission(player, null, node);
 	}
 
+	/**
+	 * Adds a Permission for a given world to a Player.
+	 * 
+	 * @param player
+	 *            Player to be modified
+	 * @param world
+	 *            World the Permission belongs to
+	 * @param node
+	 *            Permission-node to be added
+	 * @return success of operation
+	 */
 	public boolean addPlayerPermission(String player, String world, String node) {
-		return plugin.dataProvider.addPlayerPermission(fakeCS, player, world, node);
+		return plugin.dataProvider.addPlayerPermission(fakeCS, player, world,
+				node);
 	}
 
+	/**
+	 * Removes a global Permission to a Player.
+	 * 
+	 * @param player
+	 *            Player to be modified
+	 * @param node
+	 *            Permission-node to be removed
+	 * @return success of operation
+	 */
 	public boolean removePlayerPermission(String player, String node) {
 		return removeGroupPermission(player, null, node);
 	}
 
+	/**
+	 * Removes a Permission for a given world from a Player.
+	 * 
+	 * @param player
+	 *            Player to be modified
+	 * @param world
+	 *            World the Permission belongs to
+	 * @param node
+	 *            Permission-node to be removed
+	 * @return success of operation
+	 */
 	public boolean removePlayerPermission(String player, String world,
 			String node) {
-		return plugin.dataProvider.removePlayerPermission(fakeCS, player, world, node);
+		return plugin.dataProvider.removePlayerPermission(fakeCS, player,
+				world, node);
 	}
 
+	/**
+	 * Returns Data from the Players Info-node.
+	 * 
+	 * @param player
+	 *            Player to be queried
+	 * @param node
+	 *            Info-node to be read
+	 * @return Info-node
+	 */
 	public String getPlayerInfo(String player, String node) {
 		return plugin.dataProvider.getPlayerInfo(fakeCS, player, node);
 	}
 
+	/**
+	 * Sets Data in the Players Info-node.
+	 * 
+	 * @param player
+	 *            Player to be modified.
+	 * @param node
+	 *            Info-node to be set.
+	 * @param data
+	 *            Data to be written.
+	 * @return
+	 */
 	public boolean setPlayerInfo(String player, String node, String data) {
 		return plugin.dataProvider.setPlayerInfo(fakeCS, player, node, data);
 	}
 
+	/**
+	 * Returns all Subgroups of a given Group.
+	 * 
+	 * @param group
+	 *            Group to be queried
+	 * @return Array of Subgroup-Strings
+	 */
 	public String[] getGroupSubgroups(String group) {
 		return plugin.dataProvider.getGroupSubgroups(group);
 	}
 
+	/**
+	 * Adds a Subgroup to a given Group.
+	 * 
+	 * @param group
+	 *            Group to be modified
+	 * @param subgroup
+	 *            Subgroup to be added
+	 * @return success of operation
+	 */
 	public boolean addGroupSubgroup(String group, String subgroup) {
 		return plugin.dataProvider.addGroupSubgroup(fakeCS, group, subgroup);
 	}
 
+	/**
+	 * Removes a Subgroup from a given Group.
+	 * 
+	 * @param group
+	 *            Group to be modified
+	 * @param subgroup
+	 *            Subgroup to be removed
+	 * @return success of operation
+	 */
 	public boolean removeGroupSubgroup(String group, String subgroup) {
 		return plugin.dataProvider.removeGroupSubgroup(fakeCS, group, subgroup);
 	}
 
+	/**
+	 * Adds a global Permission to a Group.
+	 * 
+	 * @param group
+	 *            Group to be modified
+	 * @param node
+	 *            Permission-node to add
+	 * @return success of operation
+	 */
 	public boolean addGroupPermission(String group, String node) {
 		return addGroupPermission(group, null, node);
 	}
 
+	/**
+	 * Adds a Permission for a given world to a Group.
+	 * 
+	 * @param group
+	 *            Group to be modified
+	 * @param world
+	 *            World the Permission belongs to
+	 * @param node
+	 *            Permission-node to add
+	 * @return success of operation
+	 */
 	public boolean addGroupPermission(String group, String world, String node) {
-		return plugin.dataProvider.addGroupPermission(fakeCS, group, world, node);
+		return plugin.dataProvider.addGroupPermission(fakeCS, group, world,
+				node);
 	}
 
+	/**
+	 * Removes a global Permission from a Group. permission.
+	 * 
+	 * @param group
+	 *            Group to be modified
+	 * @param node
+	 *            Permission-node to remove
+	 * @return success of operation
+	 */
 	public boolean removeGroupPermission(String group, String node) {
 		return removeGroupPermission(group, null, node);
 	}
-	
-	public boolean removeGroupPermission(String group, String world,
-			String node) {
-		return plugin.dataProvider.removeGroupPermission(fakeCS, group, world, node);
+
+	/**
+	 * Removes a Permission for a given world from a Group. permission.
+	 * 
+	 * @param group
+	 *            Group to be modified
+	 * @param world
+	 *            World the Permission belongs to
+	 * @param node
+	 *            Permission-node to remove
+	 * @return success of operation
+	 */
+	public boolean removeGroupPermission(String group, String world, String node) {
+		return plugin.dataProvider.removeGroupPermission(fakeCS, group, world,
+				node);
 	}
-	
+
+	/**
+	 * Gets Data from the Groups Info-node.
+	 * 
+	 * @param group
+	 *            Group to be queried
+	 * @param node
+	 *            Info-node to be read
+	 * @return Info-node
+	 */
 	public String getGroupInfo(String group, String node) {
 		return plugin.dataProvider.getGroupInfo(fakeCS, group, node);
 	}
 
+	/**
+	 * Sets Data in the Groups Info-node.
+	 * 
+	 * @param group
+	 *            Group to be modified.
+	 * @param node
+	 *            Info-node to be set.
+	 * @param data
+	 *            Data to be written.
+	 * @return
+	 */
 	public boolean setGroupInfo(String group, String node, String data) {
 		return plugin.dataProvider.setGroupInfo(fakeCS, group, node, data);
 	}
--- src/main/java/de/hydrox/bukkit/DroxPerms/data/IDataProvider.java
+++ src/main/java/de/hydrox/bukkit/DroxPerms/data/IDataProvider.java
@@ -23,35 +23,39 @@ public interface IDataProvider {
 	public boolean createPlayer(String name);
 
 	/**
-	 * Creates a Group
 	 * 
+	 * @param sender
 	 * @param name
 	 *            name of the Group
+	 * 
 	 * @return returns true if the Group could be created.
 	 */
 	public boolean createGroup(CommandSender sender, String name);
-	
+
 	/**
-	 * Returns the main-group of the Player
+	 * Returns the main-group of the Player.
 	 * 
 	 * @param player
-	 *            player to be queried.
+	 *            player to be queried. sender
+	 * 
 	 * @return main group of the Player
 	 */
 	public String getPlayerGroup(String player);
 
 	/**
-	 * Sets the main-group of the Player
+	 * Sets the main-group of the Player.
 	 * 
+	 * @param sender
 	 * @param player
 	 *            player to be modified
 	 * @param subgroup
 	 *            group to be set
 	 */
-	public boolean setPlayerGroup(CommandSender sender, String player, String group);
+	public boolean setPlayerGroup(CommandSender sender, String player,
+			String group);
 
 	/**
-	 * Returns the sub-groups of the Player
+	 * Returns the sub-groups of the Player.
 	 * 
 	 * @param player
 	 *            player to be queried
@@ -60,51 +64,200 @@ public interface IDataProvider {
 	public String[] getPlayerSubgroups(String player);
 
 	/**
-	 * Adds a Sub-group to a Player
+	 * Adds a Sub-group to a Player.
 	 * 
 	 * @param player
 	 *            Player to be modified
 	 * @param subgroup
 	 *            sub-group to be added
 	 */
-	public boolean addPlayerSubgroup(CommandSender sender, String player, String subgroup);
+	public boolean addPlayerSubgroup(CommandSender sender, String player,
+			String subgroup);
 
 	/**
-	 * Removes a Sub-group from a Player
+	 * Removes a Sub-group from a Player.
 	 * 
 	 * @param player
 	 *            Player to be modified
 	 * @param subgroup
 	 *            sub-group to be removed
 	 */
-	public boolean removePlayerSubgroup(CommandSender sender, String player, String subgroup);
+	public boolean removePlayerSubgroup(CommandSender sender, String player,
+			String subgroup);
 
-	public boolean addPlayerPermission(CommandSender sender, String player, String world, String node);
+	/**
+	 * Adds a Permission to a Player. Set world to null for global permission.
+	 * 
+	 * @param sender
+	 * @param player
+	 *            Player to be modified
+	 * @param world
+	 *            World the Permission belongs to
+	 * @param node
+	 *            Permission-node to add
+	 * @return
+	 */
+	public boolean addPlayerPermission(CommandSender sender, String player,
+			String world, String node);
 
-	public boolean removePlayerPermission(CommandSender sender, String player, String world,
-			String node);
+	/**
+	 * Removes a Permission from a Player. Set world to null for global
+	 * permission.
+	 * 
+	 * @param sender
+	 * @param player
+	 *            Player to be modified
+	 * @param world
+	 *            World the Permission belongs to
+	 * @param node
+	 *            Permission-node to remove
+	 * @return
+	 */
+	public boolean removePlayerPermission(CommandSender sender, String player,
+			String world, String node);
 
+	/**
+	 * Returns all Permissions for a Player from a given world. Set world to
+	 * null for global permission.
+	 * 
+	 * @param player
+	 *            Player to be queried.
+	 * @param world
+	 *            World to be queried.
+	 * @return
+	 */
 	public String[] getPlayerPermissions(String player, String world);
 
-	public boolean setPlayerInfo(CommandSender sender, String player, String node, String data);
+	/**
+	 * Sets Data in the Players Info-node.
+	 * 
+	 * @param sender
+	 * @param player
+	 *            Player to be modified.
+	 * @param node
+	 *            Info-node to be set.
+	 * @param data
+	 *            Data to be written.
+	 * @return
+	 */
+	public boolean setPlayerInfo(CommandSender sender, String player,
+			String node, String data);
 
+	/**
+	 * Gets Data from the Players Info-node.
+	 * 
+	 * @param sender
+	 * @param player
+	 *            Player to be queried
+	 * @param node
+	 *            Info-node to be read
+	 * @return Info-node
+	 */
 	public String getPlayerInfo(CommandSender sender, String player, String node);
 
+	/**
+	 * Returns all Subgroups of a given Group.
+	 * 
+	 * @param group
+	 *            Group to be queried
+	 * @return Array of Subgroup-Strings
+	 */
 	public String[] getGroupSubgroups(String group);
 
-	public boolean addGroupSubgroup(CommandSender sender, String group, String subgroup);
+	/**
+	 * Adds a Subgroup to a given Group.
+	 * 
+	 * @param sender
+	 * @param group
+	 *            Group to be modified
+	 * @param subgroup
+	 *            Subgroup to be added
+	 * @return success of operation
+	 */
+	public boolean addGroupSubgroup(CommandSender sender, String group,
+			String subgroup);
 
-	public boolean removeGroupSubgroup(CommandSender sender, String group, String subgroup);
+	/**
+	 * Removes a Subgroup from a given Group.
+	 * 
+	 * @param sender
+	 * @param group
+	 *            Group to be modified
+	 * @param subgroup
+	 *            Subgroup to be removed
+	 * @return success of operation
+	 */
+	public boolean removeGroupSubgroup(CommandSender sender, String group,
+			String subgroup);
 
-	public boolean addGroupPermission(CommandSender sender, String group, String world, String node);
+	/**
+	 * Adds a Permission to a Group. Set world to null for global permission.
+	 * 
+	 * @param sender
+	 * @param group
+	 *            Group to be modified
+	 * @param world
+	 *            World the Permission belongs to
+	 * @param node
+	 *            Permission-node to add
+	 * @return success of operation
+	 */
+	public boolean addGroupPermission(CommandSender sender, String group,
+			String world, String node);
 
-	public boolean removeGroupPermission(CommandSender sender, String group, String world,
-			String node);
+	/**
+	 * Removes a Permission from a Group. Set world to null for global
+	 * permission.
+	 * 
+	 * @param sender
+	 * @param group
+	 *            Group to be modified
+	 * @param world
+	 *            World the Permission belongs to
+	 * @param node
+	 *            Permission-node to remove
+	 * @return success of operation
+	 */
+	public boolean removeGroupPermission(CommandSender sender, String group,
+			String world, String node);
 
+	/**
+	 * Returns all Permissions for a Group from a given world. Set world to null
+	 * for global permission.
+	 * 
+	 * @param group
+	 *            Group to be queried.
+	 * @param world
+	 *            World to be queried.
+	 * @return
+	 */
 	public String[] getGroupPermissions(String group, String world);
 
-	public boolean setGroupInfo(CommandSender sender, String group, String node, String data);
+	/**
+	 * Sets Data in the Groups Info-node.
+	 * 
+	 * @param sender
+	 * @param group
+	 *            Group to be modified.
+	 * @param node
+	 *            Info-node to be set.
+	 * @param data
+	 *            Data to be written.
+	 * @return
+	 */
+	public boolean setGroupInfo(CommandSender sender, String group,
+			String node, String data);
 
+	/**
+	 * Gets Data from the Groups Info-node.
+	 * 
+	 * @param sender
+	 * @param group
+	 *            Group to be queried
+	 * @param node
+	 *            Info-node to be read
+	 * @return Info-node
+	 */
 	public String getGroupInfo(CommandSender sender, String group, String node);
 
 	public void save();
