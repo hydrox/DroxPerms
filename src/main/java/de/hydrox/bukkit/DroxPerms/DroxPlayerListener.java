9b481ed62006b8050fb481a52e687de20e54d4e6
Matthias Söhnholz <miniminko@googlemail.com>
2012-01-29 20:12:00 +0100

    add logging time of last login and quit

--- src/main/java/de/hydrox/bukkit/DroxPerms/DroxPlayerListener.java
+++ src/main/java/de/hydrox/bukkit/DroxPerms/DroxPlayerListener.java
@@ -56,4 +56,13 @@ public class DroxPlayerListener implements Listener {
 		}
 	}
 
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void updatePlayerSeenOnLogin(PlayerLoginEvent event) {
+		plugin.getAPI().setPlayerInfo(event.getPlayer().getName(), "lastSeen", Long.toString((System.currentTimeMillis() / 1000L)));
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void updatePlayerSeenOnQuit(PlayerQuitEvent event) {
+		plugin.getAPI().setPlayerInfo(event.getPlayer().getName(), "lastSeen", Long.toString((System.currentTimeMillis() / 1000L)));
+	}
 }
